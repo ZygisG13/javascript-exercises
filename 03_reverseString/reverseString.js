c47f8451d3657e0b07199778fe72fd2459378a82
zygisG13 <zygimantas.guobuzas@gmail.com>
2022-11-29 10:03:19 +0200

    All task completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reverseString = ''
+    for (let index = stringArray.length; index > 0; index--) {
+        reverseString += stringArray.pop();
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
