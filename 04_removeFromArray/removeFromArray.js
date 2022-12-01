508aba091c2fc23b11bddd01103b03aef30c36b3
zygisG13 <zygimantas.guobuzas@gmail.com>
2022-12-01 10:41:48 +0200

    All tasks done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...elementsToRemove) {
+  //console.log(elementsToRemove);
+  for (let indexToRemove = 0; indexToRemove < elementsToRemove.length; indexToRemove++) {
+    for (let index = 0; index < array.length; index++) {
+      if (array[index] === elementsToRemove[indexToRemove]) {
+        //console.log(elementsToRemove[indexToRemove]);
+        removed = array.splice(index, 1);
+      }
+    }
+  }
+  return array;
 };
 
+//removeFromArray([1, 2, 2, 3, 4, 5], 1, 2, 5, 6)
+
 // Do not edit below this line
+
 module.exports = removeFromArray;
