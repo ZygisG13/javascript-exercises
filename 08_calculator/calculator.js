b11fc83feb617508cc1845abfdb624c3b1a93ebf
zygisG13 <zygimantas.guobuzas@gmail.com>
2022-12-19 15:29:41 +0200

    Tasks done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (myArray) {
+  if (myArray.length === 0) {
+    return 0;
+  }
+  if (myArray.length === 1) {
+    return myArray[0];
+  }
+  let a = 0;
+  for (let index = 0; index < myArray.length; index++) {
+    a = a + myArray[index];
+  }
+  return a;
 };
 
-const multiply = function() {
-
+const multiply = function (myArray) {
+  let a = myArray[0];
+  for (let index = 1; index < myArray.length; index++) {
+    a = a * myArray[index];
+  }
+  return a;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let c = a;
+  for (let index = 1; index < b; index++) {
+    a = a * c;
+  }
+  return a;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0) {
+    return 1;
+  }
+  let b = 1;
+  for (let index = 1; index <= a; index++) {
+    b = b * index;
+  }
+  return b;
 };
 
 // Do not edit below this line
@@ -29,5 +54,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
