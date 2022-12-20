a543e9c263a8539d75b5f7fe9a0a9f37a22a6360
zygisG13 <zygimantas.guobuzas@gmail.com>
2022-12-20 14:37:36 +0200

    all tasks done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function (member) {
+  const parsed = parseInt(member);
+  if (parsed === 2 || parsed === 3) {
+    return 1;
+  }
+  if (parsed < 0) {
+    return "OOPS";
+  }
+  let fibonacciArray = [1, 1];
+  let fibNext = 0;
+  for (let index = 2; index <= parsed; index++) {
+    fibNext = fibonacciArray[index - 1] + fibonacciArray[index - 2];
+    //console.log(fibNext);
+    fibonacciArray.push(fibNext);
+  }
+  //console.log(fibonacciArray[parsed-1])
+  return fibonacciArray[parsed - 1];
 };
 
+fibonacci(8);
 // Do not edit below this line
 module.exports = fibonacci;
