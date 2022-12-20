3e4be31fa245ad5fbdd8f97aac7a2bf38c7d62ab
zygisG13 <zygimantas.guobuzas@gmail.com>
2022-12-20 11:32:49 +0200

    Tasks done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (myString) {
+  myString = myString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ""); //remove puctuation
+  myString = myString.toLowerCase().replace(/\s/g, ""); //remove spaces
+  myStringArray = myString.split().join();
+  const reverseStringArray = [...myString].reverse().join(""); //reverse myStringArray copy
+  console.log(myStringArray);
+  console.log(reverseStringArray);
+  return myStringArray === reverseStringArray ? true : false;
 };
+
+//palindromes("A car, a man, a maraca.");
 
 // Do not edit below this line
 module.exports = palindromes;
