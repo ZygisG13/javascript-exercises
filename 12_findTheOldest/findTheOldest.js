8cc677b345e7a7ddb3fd601ea5c974b248a53664
zygisG13 <zygimantas.guobuzas@gmail.com>
2022-12-22 11:38:08 +0200

    Tasks done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  people.forEach(element => { 
+    if (element.yearOfDeath != undefined) {
+      element.lives = element.yearOfDeath - element.yearOfBirth;
+    }
+    else {
+      element.lives = new Date().getFullYear() - element.yearOfBirth;
+    }
 
+    });
+  //sorting objects from oldest to youngest
+  oldest = people.sort((a,b) => a.lives < b.lives ? 1 : -1)
+  console.log(oldest[0]);
+  return oldest[0]; //return first member of array
 };
 
 // Do not edit below this line
